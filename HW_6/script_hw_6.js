f5be5a49d5888f3f0ead6e9c9e4d66429f9279e7
AsteDor <134375776+Mikhail-Chkhan@users.noreply.github.com>
2024-05-15 11:47:56 +0300

    hw_6.1 - 6.10 - done

--- HW_6/script_hw_6.js
+++ HW_6/script_hw_6.js
@@ -17,7 +17,7 @@ for (const str of array) {
 
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
-console.error('Task 6.2')
+console.error('Task 6.3')
 let mas = ['HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL']
 for (const item of mas) {
     console.log(item.toLowerCase())
@@ -26,37 +26,101 @@ for (const item of mas) {
 
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 // -----------------------------------------------
-// let str = ' dirty string   '
-// let clear_space = (string_input) =>{
-//     if (string_input.start(' ')) {
-//         string_input.substring(1,7)
-//     }
-//     console.log(string_input)
-//
-// }
-// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+// debugger
+console.error('Task 6.4')
+let str = '   dirty string   '
+function clear_space (string) {
+    if (string.startsWith(' ')) {
+        string = string.replace(' ', '')
+        clear_space(string)
+    }
+    else if (string.endsWith(' ')) {
+        string = string.substring(0,string.length-1)
+        clear_space(string)
+    }
+    else {
+        return string
+    }
+}
+console.log(clear_space(str))
+// -----------------------------------------------
+const dirty = str.trim()
+console.log(dirty)
+// -----------------------------------------------
+
+// - 6.5 Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
 // let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
-//
-// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
-// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+console.error('Task 6.5')
+let str1 = 'Ревуть воли як ясла повні';
+let  stringToarray = (str) => str.split(' ')
+let arr = stringToarray(str)
+console. log (stringToarray(str1))
+/// -----------------------------------------------
+
+// - 6.6. є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+console.error('Task 6.6')
+let arr_number =  [10,8,-7,55,987,-1011,0,1050,0]
+let arr_number_string = arr_number.map (value => value.toString())
+console.log(arr_number_string)
+/// -----------------------------------------------
+
+// - 6.7 створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
+// або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
-//
-// ==========================
-// - є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// -- відсортувати його за спаданням за monthDuration
-// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-// -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+let sortNums = (arr, condition) => {
+    if (condition === 'ascending') {
+        arr.sort((num1,num2) => num1 - num2)
+        console.log(arr)
+    }else if (condition === 'descending') {
+        arr.sort((num1,num2) => {return num2 - num1})
+        console.log(arr)
+    }
+    else {
+        console.log('err')
+    }
+}
+console.error('Task 6.7')
+let nums = [11,21,3]
+sortNums(nums,'ascending') // [3,11,21]
+sortNums(nums,'descending') // [21,11,3]
+/// -----------------------------------------------
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+// - є масив coursesAndDurationArray
+// -- 6.8 відсортувати його за спаданням за monthDuration
+console.error('Task 6.8')
+coursesAndDurationArray.sort((monthD_1, monthD_2) => {
+    return (monthD_2.monthDuration - monthD_1.monthDuration)
+})
+console.log(coursesAndDurationArray)
+// -- 6.9 відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+let filter___courses =coursesAndDurationArray.filter((duration) =>{ return duration.monthDuration >5})
+console.error('Task 6.9')
+console.log(filter___courses)
+
+// -- 6.10 за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+console.error('Task 6.10')
+let list___courses = coursesAndDurationArray.map((course, index) =>{
+    // let new_course= {
+    //     id: index+1,
+    //     title: course.title,
+    //     monthDuration: course.monthDuration
+    // }
+    // return new_course
+    return {id:index+1,...course}
+} )
+console.log(list___courses)
+
 // =========================
 //     описати колоду карт (від 6 до туза без джокерів)
 // - знайти піковий туз
